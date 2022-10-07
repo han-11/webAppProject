85c115f3663daaf802e8d8f162c1d8e250310660
Heather Wang <109267651+han-11@users.noreply.github.com>
2022-10-07 20:58:22 +1300

    Delete webAppProject/assessment file directory

--- webAppProject/assessment file/connect.py
+++
@@ -1,5 +0,0 @@
-dbuser = "root"  # PUT YOUR MySQL username here - usually admin
-dbpass = "lbxx1125"  # "PUT YOUR PASSWORD HERE"
-dbhost = "localhost"  # "PUT YOUR AWS Connect String here"
-dbport = "3306"
-dbname = "airline"
